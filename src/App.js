8d125dd9b2cfedb43954521f76288b2f13a5ee4c
Kim Mun Pil <ansvlf0606@gmail.com>
2023-09-26 15:01:21 +0900

    프론트 백 연동(임시)

--- src/App.js
+++ src/App.js
@@ -1,63 +1,66 @@
 import React from 'react';
+import Todo from './Todo';
+import AddTodo from './AddTodo';
+import { Paper, List, Container } from "@material-ui/core";
 import './App.css';
-import Todo from './Todo'
-import AddTodo from './AddTodo'
-import { Container, List, Paper } from '@material-ui/core';
+import { call } from './service/ApiService';
 
 class App extends React.Component {
-  constructor(props) { //매개변수 props 생성자
-    super(props); // 매개변수 pros 초기화
-    this.state = { // item 에 item.id, item.title, item.done 매개변수 이름과 값 할당
-      items: [
-        { id: "ID-0", title: "Todo 1", done: false },
-        { id: "ID-1", title: "Todo 2", done: false }
-      ]
-    };
-  }
-  // add 함수 추가
-  add = (item) => {
-    const thisItems = this.state.items;
-    item.id = "ID-" + thisItems.length; // Key를 위한 id 추가
-    item.done = false;
-    thisItems.push(item);
-    this.setState({ items: thisItems }); // update state
-    console.log('items', this.state.items);
-  }
+    constructor(props) { //매개변수 props 생성자
+        super(props); // 매개변수 pros 초기화
+        this.state = { // item 에 item.id, item.title, item.done 매개변수 이름과 값 할당
+            items: [],
+        };
+    }
 
-  // delete 함수 추가
-  delete = (item) => {
-    const thisItems = this.state.items;
-    const newItems = thisItems.filter(e => e.id !== item.id);
-    this.setState({ items: newItems }, () => {
-      // 디버깅 콜백
-      console.log("Update Items : ", this.state.items)
-    });
-  }
+    // add 함수 추가
+    add = (item) => {
+        call("/todo", "POST", item).then((response) =>
+            this.setState({ items: response.data })
+        );
+    }
 
-  render() {
-    // 자바스크립트가 제공하는 map함수를 이용해서 배열을 반복해 <Todo /> 컴포넌트를 여러개 생성
-    // todoItems에 this.state.items.length 가 0보다 크다면 true 이므로 && 뒤에 값을 넘겨준다. 
-    // totoItem = this.state.items.length > 0 ? (<Paper></Paper>):""; 이렇게 해도 같은 결과이다. 조건선택문 ? ternary operator
-    var todoItems = this.state.items.length > 0 && (
-      <Paper style={{ margin: 16 }}>
-        <List>
-          {this.state.items.map((item, idx) => (
-            <Todo item={item} key={item.id} delete={this.delete} />
-          ))};
-        </List>
-      </Paper>
-    )
+    delete = (item) => {
+        call("/todo", "DELETE", item).then((response) =>
+            this.setState({ items: response.data })
+        );
+    }
 
-    return (
-      <div className='App'>
-        <Container maxWidth="md">
-          <AddTodo add={this.add} />
-          <div className='TodoList'>{todoItems}</div>
-        </Container>
-      </div>
-    )
-  }
+    update = (item) => {
+        call("/todo", "PUT", item).then((response) =>
+            this.setState({ items: response.data })
+        );
+    }
+
+    componentDidMount() {
+        call("/todo", "GET", null).then((response) =>
+            this.setState({ items: response.data })
+        );
+    }
+
+    render() {
+        // todoItems에 this.state.items.length 가 0보다 크다면 true 이므로 && 뒤에 값을 넘겨준다. 
+        // totoItem = this.state.items.length > 0 ? (<Paper></Paper>):""; 이렇게 해도 같은 결과이다. 조건선택문 ? ternary operator
+        var todoItems = this.state.items.length > 0 && (
+            <Paper style={{ margin: 16 }}>
+                <List>
+                    {this.state.items.map((item, idx) => (
+                        <Todo item={item} key={idx} delete={this.delete} update={this.update} />
+                    ))}
+                </List>
+            </Paper>
+        );
+
+        // 생성된 컴포넌트 JPX를 리턴한다. 
+        return (
+            <div className="App">
+                <Container maxWidth="md">
+                    <AddTodo add={this.add} />
+                    <div className="TodoList">{todoItems}</div>
+                </Container>
+            </div>
+        );
+    }
 }
-
 
 export default App;
--- src/Todo.js
+++ src/Todo.js
@@ -7,6 +7,7 @@ class Todo extends React.Component {
         super(props);
         this.state = { item: props.item, readOnly: true }; // 매개변수 item 의 변수/값을 item에 대입
         this.delete = props.delete;
+        this.update = props.update;
     }
 
     deleteEventHandler = () => {
@@ -14,7 +15,6 @@ class Todo extends React.Component {
     }
 
     offReadnOnleyMode = () => {
-        console.log("Event!", this.state.readOnly);
         this.setState({ readOnly: false }, () => {
             console.log("ReadOnly?", this.state.readOnly);
         });
@@ -23,6 +23,7 @@ class Todo extends React.Component {
     enterKeyEventHandler = (e) => {
         if (e.key === 'Enter') {
             this.setState({ readOnly: true });
+            this.update(this.state.item);
         }
     }
 
@@ -36,7 +37,8 @@ class Todo extends React.Component {
         console.log("check box event call");
         const thisItem = this.state.item;
         thisItem.done = thisItem.done ? false : true;
-        this.setState({ item: thisItem });
+        this.setState({readOnly:true});
+        this.update(this.state.item);
     }
 
     render() {
