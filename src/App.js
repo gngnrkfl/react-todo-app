dd7c4df31d1b64c6ca7ef35f1603a2966b2cd0de
Kim Mun Pil <ansvlf0606@gmail.com>
2023-09-26 11:14:56 +0900

    11장 React로 목록 추가, 수정, 삭제하기

--- src/App.js
+++ src/App.js
@@ -9,19 +9,31 @@ class App extends React.Component {
     super(props); // 매개변수 pros 초기화
     this.state = { // item 에 item.id, item.title, item.done 매개변수 이름과 값 할당
       items: [
-        { id: 0, title: "Todo 1", done: false },
-        { id: 1, title: "Todo 2", done: false }
+        { id: "ID-0", title: "Todo 1", done: false },
+        { id: "ID-1", title: "Todo 2", done: false }
       ]
     };
-  } 
+  }
+  // add 함수 추가
   add = (item) => {
     const thisItems = this.state.items;
     item.id = "ID-" + thisItems.length; // Key를 위한 id 추가
     item.done = false;
     thisItems.push(item);
-    this.setState({items:thisItems}); // update state
+    this.setState({ items: thisItems }); // update state
     console.log('items', this.state.items);
   }
+
+  // delete 함수 추가
+  delete = (item) => {
+    const thisItems = this.state.items;
+    const newItems = thisItems.filter(e => e.id !== item.id);
+    this.setState({ items: newItems }, () => {
+      // 디버깅 콜백
+      console.log("Update Items : ", this.state.items)
+    });
+  }
+
   render() {
     // 자바스크립트가 제공하는 map함수를 이용해서 배열을 반복해 <Todo /> 컴포넌트를 여러개 생성
     // todoItems에 this.state.items.length 가 0보다 크다면 true 이므로 && 뒤에 값을 넘겨준다. 
@@ -30,7 +42,7 @@ class App extends React.Component {
       <Paper style={{ margin: 16 }}>
         <List>
           {this.state.items.map((item, idx) => (
-            <Todo item={item} key={idx} />
+            <Todo item={item} key={item.id} delete={this.delete} />
           ))};
         </List>
       </Paper>
@@ -39,7 +51,7 @@ class App extends React.Component {
     return (
       <div className='App'>
         <Container maxWidth="md">
-          <AddTodo add={this.add}/>
+          <AddTodo add={this.add} />
           <div className='TodoList'>{todoItems}</div>
         </Container>
       </div>
--- src/Todo.js
+++ src/Todo.js
@@ -1,26 +1,68 @@
-import { Checkbox, InputBase, ListItem, ListItemText } from '@material-ui/core';
+import { Checkbox, IconButton, InputBase, ListItem, ListItemSecondaryAction, ListItemText } from '@material-ui/core';
+import { DeleteOutline } from '@material-ui/icons';
 import React from 'react';
 
 class Todo extends React.Component {
     constructor(props) {
-        super(props); 
-        this.state = { item: props.item }; // 매개변수 item 의 변수/값을 item에 대입
-    } render() {
+        super(props);
+        this.state = { item: props.item, readOnly: true }; // 매개변수 item 의 변수/값을 item에 대입
+        this.delete = props.delete;
+    }
+
+    deleteEventHandler = () => {
+        this.delete(this.state.item);
+    }
+
+    offReadnOnleyMode = () => {
+        console.log("Event!", this.state.readOnly);
+        this.setState({ readOnly: false }, () => {
+            console.log("ReadOnly?", this.state.readOnly);
+        });
+    }
+
+    enterKeyEventHandler = (e) => {
+        if (e.key === 'Enter') {
+            this.setState({ readOnly: true });
+        }
+    }
+
+    editEventHandler = (e) => {
+        const thisItem = this.state.item;
+        thisItem.title = e.target.value;
+        this.setState({ item: thisItem });
+    }
+
+    checkboxEventHandler = (e) => {
+        console.log("check box event call");
+        const thisItem = this.state.item;
+        thisItem.done = thisItem.done ? false : true;
+        this.setState({ item: thisItem });
+    }
+
+    render() {
         const item = this.state.item;
         return (
             <ListItem>
-                <Checkbox checked={item.done}/>
+                <Checkbox checked={item.done} onChange={this.checkboxEventHandler} />
                 <ListItemText>
-                    <InputBase 
-                    inputProps={{"aria-label" : "naked"}}
-                    type='text'
-                    id={item.id}
-                    name={item.id}
-                    value={item.title}
-                    multiline={true}
-                    fullWidth={true}
+                    <InputBase
+                        inputProps={{ "aria-label": "naked", readOnly: this.state.readOnly }}
+                        type='text'
+                        id={item.id}
+                        name={item.id}
+                        value={item.title}
+                        multiline={true}
+                        fullWidth={true}
+                        onClick={this.offReadnOnleyMode}
+                        onChange={this.editEventHandler}
+                        onKeyDown={this.enterKeyEventHandler}
                     />
                 </ListItemText>
+                <ListItemSecondaryAction>
+                    <IconButton aria-label='Delete' onClick={this.deleteEventHandler}>
+                        <DeleteOutline />
+                    </IconButton>
+                </ListItemSecondaryAction>
             </ListItem>
         );
     }
