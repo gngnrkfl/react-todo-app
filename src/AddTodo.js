2318279f14366405d8586cbd3eba990ae06c848e
Kim Mun Pil <ansvlf0606@gmail.com>
2023-09-26 14:19:06 +0900

    함수형 컴포넌트 변경

--- src/AddTodo.js
+++ src/AddTodo.js
@@ -1,57 +1,49 @@
 import { Button, Grid, Paper, TextField } from "@material-ui/core";
-import React from "react";
+import React, { useState } from "react";
 
-class AddTodo extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = { item: { title: "" } };
-        this.add = props.add; // props의 함수를 this.add에 연결, props에는 상위 컴포넌트(App.js)의 함수 매개변수가 들어있음
+const AddTodo = (props) => {
+    const [item, setItem] = useState("");
+
+    function onInputChange(e) {
+        setItem(e.target.value);
     }
 
-    onInputChange = (e) => {
-        const thisItem = this.state.item;
-        thisItem.title = e.target.value;
-        this.setState({ item: thisItem });
-        console.log(thisItem);
+    function onButtonClick() {
+        let addItem = { id: "", title: item, done: false };
+        props.add(addItem); // 텍스트 값을 추가하고
+        setItem("");
     }
 
-    onButtonClick = () => {
-        this.add(this.state.item); // 텍스트 값을 추가하고
-        this.setState({ item: { title: "" } }); // 입력필드는 초기화
-    }
-
-    enterKeyEventHandler = (e) => {
+    function enterKeyEventHandler(e) {
         if (e.key === 'Enter') {
             this.onButtonClick();
         }
     }
 
-    render() {
-        return (
-            <Paper style={{ margin: 16, padding: 16 }}>
-                <Grid container>
-                    <Grid xs={11} md={11} item style={{ paddingRight: 16 }}>
-                        <TextField
-                            placeholder="Add Todo here"
-                            fullWidth
-                            onChange={this.onInputChange}
-                            value={this.state.item.title}
-                            onKeyDown={this.enterKeyEventHandler}
-                        />
-                    </Grid>
-                    <Grid xs={1} md={1} item>
-                        <Button
-                            fullWidth
-                            color="secondary"
-                            variant="outlined"
-                            onClick={this.onButtonClick}>
-                            +
-                        </Button>
-                    </Grid>
+    return (
+        <Paper style={{ margin: 16, padding: 16 }}>
+            <Grid container>
+                <Grid xs={11} md={11} item style={{ paddingRight: 16 }}>
+                    <TextField
+                        placeholder="Add Todo here"
+                        fullWidth
+                        onChange={onInputChange}
+                        value={item}
+                        onKeyDown={enterKeyEventHandler}
+                    />
                 </Grid>
-            </Paper>
-        )
-    }
+                <Grid xs={1} md={1} item>
+                    <Button
+                        fullWidth
+                        color="secondary"
+                        variant="outlined"
+                        onClick={onButtonClick}>
+                        +
+                    </Button>
+                </Grid>
+            </Grid>
+        </Paper>
+    )
 }
 
 export default AddTodo;
--- src/Todo.js
+++ src/Todo.js
@@ -1,71 +1,64 @@
 import { Checkbox, IconButton, InputBase, ListItem, ListItemSecondaryAction, ListItemText } from '@material-ui/core';
 import { DeleteOutline } from '@material-ui/icons';
-import React from 'react';
+import React, { useState } from 'react';
 
-class Todo extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = { item: props.item, readOnly: true }; // 매개변수 item 의 변수/값을 item에 대입
-        this.delete = props.delete;
+const Todo = (props) => {
+    const [state, setState] = useState({ item : props.item, readOnly: true });
+
+    function deleteEventHandler() {
+        props.delete(state.item);
     }
 
-    deleteEventHandler = () => {
-        this.delete(this.state.item);
+    function offReadnOnleyMode(e) {
+        console.log("Event!", state.readOnly);
+        setState({ ...state, readOnly: false });
     }
 
-    offReadnOnleyMode = () => {
-        console.log("Event!", this.state.readOnly);
-        this.setState({ readOnly: false }, () => {
-            console.log("ReadOnly?", this.state.readOnly);
-        });
-    }
-
-    enterKeyEventHandler = (e) => {
+    function enterKeyEventHandler(e) {
         if (e.key === 'Enter') {
-            this.setState({ readOnly: true });
+            setState({ ...state, readOnly: true });
         }
     }
 
-    editEventHandler = (e) => {
-        const thisItem = this.state.item;
+    function editEventHandler(e) {
+        const thisItem = state.item;
         thisItem.title = e.target.value;
-        this.setState({ item: thisItem });
+        setState({ ...state, item: thisItem });
     }
 
-    checkboxEventHandler = (e) => {
+    function checkboxEventHandler(e) {
         console.log("check box event call");
-        const thisItem = this.state.item;
+        const thisItem = state.item;
         thisItem.done = thisItem.done ? false : true;
-        this.setState({ item: thisItem });
+        setState({ ...state, item: thisItem });
     }
 
-    render() {
-        const item = this.state.item;
-        return (
-            <ListItem>
-                <Checkbox checked={item.done} onChange={this.checkboxEventHandler} />
-                <ListItemText>
-                    <InputBase
-                        inputProps={{ "aria-label": "naked", readOnly: this.state.readOnly }}
-                        type='text'
-                        id={item.id}
-                        name={item.id}
-                        value={item.title}
-                        multiline={true}
-                        fullWidth={true}
-                        onClick={this.offReadnOnleyMode}
-                        onChange={this.editEventHandler}
-                        onKeyDown={this.enterKeyEventHandler}
-                    />
-                </ListItemText>
-                <ListItemSecondaryAction>
-                    <IconButton aria-label='Delete' onClick={this.deleteEventHandler}>
-                        <DeleteOutline />
-                    </IconButton>
-                </ListItemSecondaryAction>
-            </ListItem>
-        );
-    }
+    var item = state.item;
+    return (
+        <ListItem>
+            <Checkbox checked={item.done} onChange={checkboxEventHandler} />
+            <ListItemText>
+                <InputBase
+                    inputProps={{ "aria-label": "naked", readOnly: state.readOnly }}
+                    type='text'
+                    id={item.id}
+                    name={item.id}
+                    value={item.title}
+                    multiline={true}
+                    fullWidth={true}
+                    onClick={offReadnOnleyMode}
+                    onChange={editEventHandler}
+                    onKeyDown={enterKeyEventHandler}
+                />
+            </ListItemText>
+            <ListItemSecondaryAction>
+                <IconButton aria-label='Delete' onClick={deleteEventHandler}>
+                    <DeleteOutline />
+                </IconButton>
+            </ListItemSecondaryAction>
+        </ListItem>
+    );
 }
+
 
 export default Todo;
