64ad2f10463c0f0e4fa6824072bee1f94c4e9920
Kim Mun Pil <ansvlf0606@gmail.com>
2023-10-04 16:09:35 +0900

    cookie를 이용해서 아이디 저장기능 완성

--- src/Login.js
+++ src/Login.js
@@ -1,8 +1,14 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { signin } from "./service/ApiService";
-import { Button, Container, Grid, Link, TextField, Typography } from "@material-ui/core";
+import { Button, Checkbox, Container, Grid, Link, TextField, Typography } from "@material-ui/core";
+import { useCookies } from "react-cookie";
 
 const Login = (props) => {
+    const [userid, setUserid] = useState("");
+    /*====== Cookie 관련 선언======*/
+    const [cookies, setCookie, removeCookie] = useCookies(["rememberUserId"]); //Cookies 이름
+    const [isRemember, setIsRemember] = useState(false); //아이디 저장 체크박스 체크 유무
+
     function handleSubmit(e) {
         e.preventDefault();
         const data = new FormData(e.target);
@@ -12,6 +18,25 @@ const Login = (props) => {
         // ApService의 signin 메소드를 사용해 로그인
         signin({ email: email, password: password });
     }
+
+    function handleOnChange(e) {
+        setIsRemember(e.target.checked);
+        console.log(userid);
+        if (e.target.checked) {
+            setCookie('rememberUserId', userid, { maxAge: 2000 });
+        } else {
+            removeCookie('rememberUserId');
+        }
+    }
+
+    useEffect(() => {
+        console.log(cookies.rememberUserId);
+        /*저장된 쿠키값이 있으면, CheckBox TRUE 및 UserID에 값 셋팅*/
+        if (cookies.rememberUserId !== undefined) {
+            setUserid(cookies.rememberUserId);
+            setIsRemember(true);
+        }
+    }, []);
 
     return (
         <Container component="main" maxWidth="xs" style={{ marginTop: "8%" }}>
@@ -32,6 +57,8 @@ const Login = (props) => {
                             label="이메일 주소"
                             name="email"
                             autoComplete="email"
+                            value={userid}
+                            onChange={(e) => {setUserid(e.target.value)}}
                         />
                     </Grid>
                     <Grid item xs={12}>
@@ -44,6 +71,10 @@ const Login = (props) => {
                             name="password"
                             autoComplete="password"
                         />
+                    </Grid>
+                    <Grid item xs={12}>
+                        <Checkbox checked={isRemember} onChange={handleOnChange} />
+                        아이디 저장
                     </Grid>
                     <Grid item xs={12}>
                         <Button
