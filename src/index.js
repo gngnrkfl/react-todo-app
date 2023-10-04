66c122723573c1463deea9dd66df31c1ba0abb2d
Kim Mun Pil <ansvlf0606@gmail.com>
2023-10-04 10:17:09 +0900

    15장 클라이언트 사이드 렌더링, JWT Token, Spring Security를 이용한 로그인 로직 구현

--- src/index.js
+++ src/index.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import AppRouter from './AppRouter';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <AppRouter />
+  </React.StrictMode>, 
   document.getElementById('root')
 );
+
 reportWebVitals();
--- src/service/ApiService.js
+++ src/service/ApiService.js
@@ -1,21 +1,28 @@
 import { API_BASE_URL } from "../app-config";
+
 export function call(api, method, request) {
     let options = {
-        headers: new Headers({
-            "Content-Type": "application/json",
-        }),
+        headers: new Headers({ "Content-Type": "application/json", }),
         url: API_BASE_URL + api,
         method: method,
     };
     if (request) {
         options.body = JSON.stringify(request);
     }
-    return fetch(options.url, options).then((response) =>
-        response.json().then((json) => {
-            if (!response.ok) {
-                return Promise.reject(json);
+    return fetch(options.url, options)
+        .then((response) =>
+            response.json().then((json) => {
+                if (!response.ok) {
+                    return Promise.reject(json);
+                }
+                return json;
+            })
+        )
+        .catch((error) => {
+            console.log(error.status);
+            if (error.status === 403) {
+                window.location.href = "/login";
             }
-            return json;
-        })
-    );
+            return Promise.reject(error);
+        });
 }
